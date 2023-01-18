26d09668a8b665a4fcaea7e862c4f66ae750c119
cckarabatsos <chris.karabatsos5@gmail.com>
2023-01-17 21:20:18 -0500

    Created Registration Button

--- soen390_web/src/pages/MainTitle.jsx
+++ soen390_web/src/pages/MainTitle.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LoginDialog from "../components/LoginDialog";
+import Register from "../components/Register"
 
 function MainTitle(props) {
   return (
     <>
       <h1>Main</h1>
       <LoginDialog />
+      <Register />
     </>
   );
 }
