f20eeed07b5c6c4eccfda4273c14bf596a3b0c46
Bogdan Podariu <bpodariu@genetec.com>
2023-01-17 19:45:37 -0500

    set up folders and navigation between different pages

--- SOEN390_APP/App.tsx
+++ SOEN390_APP/App.tsx
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './Pages/Login';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}: {navigation: any}) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+
+const ProfileScreen = () => {
+  return <Login></Login>;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ SOEN390_APP/Pages/Login.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Login(){
+
+    return(
+        <View>
+        <Text>welcome to the login page</Text>
+    </View>
+
+    );
+    
+
+
+}
