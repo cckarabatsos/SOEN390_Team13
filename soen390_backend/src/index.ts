d605acfe97ef75c182bd056c10016f77c939ac97
MatthewBeaulieu0 <78886446+MatthewBeaulieu0@users.noreply.github.com>
2023-01-21 14:48:16 -0500

    Moved files to allow for TS

---
+++ soen390_backend/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response } from "express";
+import { initializeApp } from "firebase-admin/app";
+
+//import jsonwebtoken from "jsonwebtoken";
+//import { hash, compare } from "bcrypt";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app: Application = express();
+const port = 7000;
+// const router:Router = express.Router();
+
+initializeApp({});
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: ["http;//localohost:3000"],
+    })
+);
+
+//Database
+
+//Heartbeat Route
+app.get("/hi", (_: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
