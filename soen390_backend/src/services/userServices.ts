ef8ad534cd8a0ca87e278cbd83c339b8b9e5548b
MatthewBeaulieu0 <78886446+MatthewBeaulieu0@users.noreply.github.com>
2023-01-24 14:09:35 -0500

    Changed some code for bog

--- soen390_backend/src/services/userServices.ts
+++ soen390_backend/src/services/userServices.ts
@@ -2,13 +2,14 @@
 import firebase from "firebase";
 const db = firebase.firestore();
 export async function findUserWithID(userID: string) {
-    //const userRef = await db.collection("users").doc(userID).get();
+    const user = "";
+    // const userRef = await db.collection("users").doc(userID).get();
     // Small bug just need to get one document
-    // console.log(userID);
-    // const userRef = db.collection("users");
-    // const snapshot = await userRef.get();
-    // snapshot.forEach((doc) => {
-    //     console.log(doc.id, "=>", doc.data());
-    // });
+    console.log(userID);
+    const userRef = db.collection("users");
+    const snapshot = await userRef.get();
+    snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+    });
     return user;
 }
