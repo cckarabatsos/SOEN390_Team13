0dc5b2e37fb3c09e4958538bcbba0b8de274898d
Bogdan Podariu <bpodariu@genetec.com>
2023-01-24 14:41:53 -0500

    So the user query by id

--- soen390_backend/src/services/userServices.ts
+++ soen390_backend/src/services/userServices.ts
@@ -1,15 +1,8 @@
-// const UserDB = require("../firebaseconfig.ts");
+//thx saad
 import firebase from "firebase";
 const db = firebase.firestore();
-export async function findUserWithID(userID: string) {
-    const user = "";
-    // const userRef = await db.collection("users").doc(userID).get();
-    // Small bug just need to get one document
-    console.log(userID);
-    const userRef = db.collection("users");
-    const snapshot = await userRef.get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, "=>", doc.data());
-    });
-    return user;
-}
+
+export const findUserWithID = async (userID: string) => {
+  const snapShot = await db.collection("users").doc(userID).get();
+  return snapShot.data();
+};
