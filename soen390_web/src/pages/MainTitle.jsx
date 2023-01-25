76c9fbab8e02687e9903edb35b2bb22b2d9d6777
khandragon <saaadkhan23@yahoo.ca>
2023-01-24 22:34:31 -0500

    Revert "created navbar"

--- soen390_web/src/pages/MainTitle.jsx
+++ soen390_web/src/pages/MainTitle.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import LoginDialog from "../components/LoginDialog";
-import NavBar from "../components/NavBar";
 
 function MainTitle(props) {
   return (
     <>
-      <NavBar />
       <h1>Main</h1>
       <LoginDialog />
     </>
