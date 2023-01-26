df5a35b670ccccc32b3923c7b06490abe329d619
cckarabatsos <chris.karabatsos5@gmail.com>
2023-01-26 16:41:39 -0500

    App

--- soen390_web/src/App.js
+++ soen390_web/src/App.js
@@ -1,27 +1,18 @@
 import "../src/App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Signup from "./pages/Signup";
-=======
-import MainTitle from "./pages/MainTitle";
-
->>>>>>> dev
 
 const AppWrapper = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/Signup" element={<Signup/>} />
-=======
-          <Route path="/" element={<MainTitle />} />
->>>>>>> dev
         </Routes>
       </Router>
     </div>
