8f67b0dd53bba4113c2903e38469c4c6ce236bae
Bogdan Podariu <podariubogdan1@gmail.com>
2023-01-26 14:55:19 -0500

    merge with backend files

---
+++ soen390_backend/dist/controllers/userControllers.d.ts
@@ -0,0 +1,3 @@
+export declare function getUserWithID(userID: string): Promise<(string | number)[] | (number | {
+    msg: string;
+})[]>;
---
+++ soen390_backend/dist/models/User.d.ts
@@ -0,0 +1,3 @@
+import * as yup from "yup";
+export declare const user_schema: any;
+export type User = yup.InferType<typeof user_schema>;
