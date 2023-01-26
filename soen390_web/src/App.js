74588e0032540afd6aa8cfea860721867c418847
cckarabatsos <chris.karabatsos5@gmail.com>
2023-01-26 16:55:51 -0500

    Fixing Changes

--- soen390_web/src/App.js
+++ soen390_web/src/App.js
@@ -2,7 +2,7 @@ import "../src/App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+
 import Signup from "./pages/Signup";
 
 const AppWrapper = () => {
@@ -11,7 +11,6 @@ const AppWrapper = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/Signup" element={<Signup/>} />
         </Routes>
       </Router>
