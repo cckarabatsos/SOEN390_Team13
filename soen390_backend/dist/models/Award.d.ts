1ee495b65ac2c8e32857ee98f46ad9b76a7c9f48
MatthewBeaulieu0 <78886446+MatthewBeaulieu0@users.noreply.github.com>
2023-01-27 20:15:53 -0500

    Revert the dist folder
    This reverts commit 475fa10375795e73dcfcaf5e7343eaf52cd8c447.

--- soen390_backend/dist/models/Award.d.ts
+++
@@ -1,3 +0,0 @@
-import * as yup from "yup";
-export declare const award_schema: any;
-export type Award = yup.InferType<typeof award_schema>;
--- soen390_backend/dist/models/Post.d.ts
+++
@@ -1,3 +0,0 @@
-import * as yup from "yup";
-export declare const post_schema: any;
-export type Post = yup.InferType<typeof post_schema>;
