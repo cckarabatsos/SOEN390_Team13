0aedbd87b1272ff3f3fba9c961a2e34e9e255819
Saad Khan <saaadkhan23@yahoo.ca>
2023-01-28 23:26:35 -0500

    tested branch

--- soen390_web/src/components/NavBar.jsx
+++ soen390_web/src/components/NavBar.jsx
@@ -10,14 +10,11 @@ import HomeIcon from "@mui/icons-material/Home";
 import NorthIcon from "@mui/icons-material/North";
 import OutputIcon from "@mui/icons-material/Output";
 import WorkIcon from "@mui/icons-material/Work";
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import navlogo from "../assets/linkedout logo navbar.png";
 import "../styles/components/navbar.css";
 import DrawerComponent from "./Drawer";
-
-
 
 function Navbar({}) {
   const theme = useTheme();
@@ -39,42 +36,47 @@ function Navbar({}) {
   };
 
   return (
-    <div data-testid='navbar-1'>
-    <AppBar position="static" style={{ background: "#BDBABA" }}>
-      <CssBaseline />
-      <Toolbar>
-        <img className="logo" src={navlogo} alt="LinkedOut" />
-        {isMobile ? (
-          <DrawerComponent />
-        ) : (
-          <span className="navlinks">
-            <Link data-testid='home-1' to="/" className="link">
-              Home <HomeIcon className="icon" sx={{ color: "#9606D9" }} />
-            </Link>
-            <Link data-testid='job-1' to="/Jobs" className="link">
-              Jobs <WorkIcon className="icon" sx={{ color: "#9606D9" }} />
-            </Link>
-
-            {!userData ? (
-              <>
-                <Link data-testid='login-1' to="/UserProfile" className="link">
-                  Login <EastIcon className="icon" sx={{ color: "#9606D9" }} />
-                </Link>
-                <Link data-testid='signup-1' to="/Signup" className="link">
-                  Signup
-                  <NorthIcon className="icon" sx={{ color: "#9606D9" }} />
-                </Link>
-              </>
-            ) : (
-              <Link to="/" className="link" onClick={signout}>
-                Signout
-                <OutputIcon className="icon" sx={{ color: "#9606D9" }} />
+    <div data-testid="navbar-1">
+      <AppBar position="static" style={{ background: "#BDBABA" }}>
+        <CssBaseline />
+        <Toolbar>
+          <img className="logo" src={navlogo} alt="LinkedOut" />
+          {isMobile ? (
+            <DrawerComponent />
+          ) : (
+            <span className="navlinks">
+              <Link data-testid="home-1" to="/" className="link">
+                Home <HomeIcon className="icon" sx={{ color: "#9606D9" }} />
               </Link>
-            )}
-          </span>
-        )}
-      </Toolbar>
-    </AppBar>
+              <Link data-testid="job-1" to="/" className="link">
+                Jobs <WorkIcon className="icon" sx={{ color: "#9606D9" }} />
+              </Link>
+
+              {!userData ? (
+                <>
+                  <Link
+                    data-testid="login-1"
+                    to="/UserProfile"
+                    className="link"
+                  >
+                    Login{" "}
+                    <EastIcon className="icon" sx={{ color: "#9606D9" }} />
+                  </Link>
+                  <Link data-testid="signup-1" to="/Signup" className="link">
+                    Signup
+                    <NorthIcon className="icon" sx={{ color: "#9606D9" }} />
+                  </Link>
+                </>
+              ) : (
+                <Link to="/" className="link" onClick={signout}>
+                  Signout
+                  <OutputIcon className="icon" sx={{ color: "#9606D9" }} />
+                </Link>
+              )}
+            </span>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
