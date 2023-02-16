60de1a4bda4ce0faa3a5b4ca983f400bd69a9f00
Bogdan Podariu <podariubogdan1@gmail.com>
2023-02-15 19:58:28 -0500

    delete duplicate update resume

--- soen390_backend/src/routes/userRoutes.ts
+++ soen390_backend/src/routes/userRoutes.ts
@@ -169,33 +169,7 @@ user.post(
         }
     }
 );
-user.post(
-    "/uploadResume/:userID",
-    upload.single("file"),
-    async (req: Request, res: Response) => {
-        let userID = req.params.userID;
-        if (req.file !== undefined) console.log(req.file.mimetype);
-        else console.log("File undefined");
 
-        try {
-            let status,
-                data = await uploadAccountFile(userID, req.file);
-
-            if (status == 200) {
-                res.status(200);
-                res.json({
-                    Response: "Successful file storage",
-                    data,
-                });
-            } else if (status == 404) {
-                res.sendStatus(404);
-            }
-        } catch (err: any) {
-            res.status(400);
-            res.json({ errType: err.Name, errMsg: err.message });
-        }
-    }
-);
 user.post("/edit/:email", async (req: Request, res: Response) => {
     try {
         let email: string = req.params.email;
