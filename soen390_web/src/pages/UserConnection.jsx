c26f9aa3fc7414d51318cb5528fddb6bfb422915
Bogdan Podariu <podariubogdan1@gmail.com>
2023-02-18 10:49:56 -0500

    remove unused imports

--- soen390_web/src/pages/UserConnection.jsx
+++ soen390_web/src/pages/UserConnection.jsx
@@ -4,18 +4,6 @@ import Footer from "../components/Footer";
 import "../styles/components/userconnection.css";
 import { Button } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
-import Person from "../assets/UserConnectionImages/image.jpg";
-import Person1 from "../assets/UserConnectionImages/image (1).jpg";
-import Person2 from "../assets/UserConnectionImages/image (2).jpg";
-import Person3 from "../assets/UserConnectionImages/image (3).jpg";
-import Person4 from "../assets/UserConnectionImages/image (4).jpg";
-import Person5 from "../assets/UserConnectionImages/image (5).jpg";
-import Person6 from "../assets/UserConnectionImages/image (6).jpg";
-import Person7 from "../assets/UserConnectionImages/image (7).jpg";
-import Person8 from "../assets/UserConnectionImages/image (8).jpg";
-import Person9 from "../assets/UserConnectionImages/image (9).jpg";
-import Person10 from "../assets/UserConnectionImages/image (10).jpg";
-import Person11 from "../assets/UserConnectionImages/image (11).jpg";
 import UserConnectionComponent from "../components/UserConectionComponent";
 import {
   GetPendingInvitations,
