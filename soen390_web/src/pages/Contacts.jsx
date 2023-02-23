766c7f7a4724fdb89baffab9cb77a1fb05c12328
Bogdan Podariu <podariubogdan1@gmail.com>
2023-02-22 20:43:16 -0500

    create page with required rights

---
+++ soen390_web/src/pages/Contacts.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contacts(){
+
+    return(
+        <div>
+            Contacts list page
+        </div>
+    )
+}
---
+++ soen390_web/src/pages/MessagingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MessagingPage(){
+
+    return(
+        <div>
+            A meesage page with one group chat
+        </div>
+    )
+}
