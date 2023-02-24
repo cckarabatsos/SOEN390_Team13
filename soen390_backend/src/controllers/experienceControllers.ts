3b92d16fdd7df64840df7a62ee6ddfb6a19b5228
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2023-02-24 16:46:29 -0500

    Created createExperience() method in experience controller

--- soen390_backend/src/controllers/experienceControllers.ts
+++ soen390_backend/src/controllers/experienceControllers.ts
@@ -1,2 +1,31 @@
 import { Experience, experience_schema } from "../models/Experience";
-import { } from "../services/experienceServices";
+import {
+    storeExperience,
+    deleteExperienceWithId,
+    retrieveExperiences
+} from "../services/experienceServices";
+
+export async function createExperience(
+    atPresent: boolean,
+    startDate: string,
+    endDate: string,
+    company: string,
+    position: string,
+    type: string,
+    ownerID: string
+) {
+    try {
+        let newExperience: Experience = experience_schema.cast({
+            atPresent, startDate, endDate, company, position, type, ownerID
+        });
+        let experience = await storeExperience(newExperience);
+        if (experience) {
+            return [200, experience];
+        }
+        else {
+            return [404, { msg: "Experience not stored" }];
+        }
+    } catch (err: any) {
+        throw err;
+    }
+}
