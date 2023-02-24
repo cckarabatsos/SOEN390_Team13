0ff1f463f133f9e852abdac0dfde8c12210e9198
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2023-02-24 16:25:09 -0500

    Created service storeExperience

--- soen390_backend/src/services/experienceServices.ts
+++ soen390_backend/src/services/experienceServices.ts
@@ -14,3 +14,22 @@ export const findExperienceWithID = async (experienceID: string) => {
     }
     return snapShot.data();
 };
+export const storeExperience = async (experience: Experience) => {
+    try {
+        var document = await db.collection("experiences").add({
+            atPresent: experience.atPresent,
+            startDate: experience.startDate,
+            endDate: experience.endDate,
+            company: experience.company,
+            position: experience.position,
+            type: experience.type,
+            ownerID: experience.ownerID
+        });
+        await document.update({ experienceID: document.id });
+        console.log("Experience successfully stored with id: " + document.id);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    return document.id;
+};
