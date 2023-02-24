76ff99010805e3cfce5935acd64897be9f151d9e
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2023-02-24 11:04:45 -0500

    Created /api/skill/:ownerID post route in skill routes

---
+++ soen390_backend/src/routes/skillRoutes.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import { deleteSkill, createSkill } from "../controllers/skillControllers";
+import { Skill } from "../models/Skill";
+const skill = express.Router();
+skill.use(express.json());
+
+skill.post("/api/skill/:ownerID", async (req: Request, res: Response) => {
+    let ownerID: string = req.params.ownerID;
+    let name: string = req.body.name;
+    try {
+        const skill: Skill = await createSkill(name, ownerID);
+        const status: number = skill[0];
+        if (status == 200) {
+            res.status(200);
+            res.json({
+                Response: "Success",
+                skill,
+            });
+        } else if (status !== 404) {
+            res.sendStatus(status);
+        }
+    } catch (err: any) {
+        res.status(400);
+        res.json({ errType: err.Name, errMsg: err.message });
+    }
+});
