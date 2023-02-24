bbe5632c02bb39d8b122f9f877d036c36d45de33
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2023-02-24 16:21:24 -0500

    Created findExperienceWithId service

--- soen390_backend/src/services/experienceServices.ts
+++ soen390_backend/src/services/experienceServices.ts
@@ -4,3 +4,13 @@ import "firebase/storage";
 import { Experience, /*experience_schema*/ } from "../models/Experience";
 
 const db = firebase.firestore();
+
+export const findExperienceWithID = async (experienceID: string) => {
+    try {
+        var snapShot = await db.collection("experiences").doc(experienceID).get();
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    return snapShot.data();
+};
