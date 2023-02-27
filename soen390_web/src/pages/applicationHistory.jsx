915696c076c6756f00a56c8c02727c03e34d06a0
cemichaud <90853959+cemichaud@users.noreply.github.com>
2023-02-27 01:27:35 -0500

    updated application history

--- soen390_web/src/pages/applicationHistory.jsx
+++ soen390_web/src/pages/applicationHistory.jsx
@@ -20,7 +20,7 @@ import "../styles/components/applicationHistory.css";
   );
 } */
 
-function ApplicationHistory() {
+ /* function ApplicationHistory() {
   const position = 'software engineer';
   const company = 'BRP';
   const location = 'mtl';
@@ -43,7 +43,76 @@ function ApplicationHistory() {
       </div>
     </div>
   );
+} */
+
+
+
+
+function ApplicationHistory() {
+  const positions = ["Software Engineer", "Frontend Developer", "Backend Developer"];
+  const companies = ["BRP", "Google", "Microsoft"];
+  const locations = ["Montreal", "San Francisco", "Seattle"];
+  const contracts = ["$100,000", "$120,000", "$150,000"];
+
+  const [showOptions, setShowOptions] = useState(Array(positions.length).fill(false));
+
+  const handleOptionsClick = (index) => {
+    const newOptions = [...showOptions];
+    newOptions[index] = !newOptions[index];
+    setShowOptions(newOptions);
+  };
+
+  return (
+    <div className="application-history-container">
+      <h1>My Applications</h1>
+      <h2>Thank you for applying to these positions</h2>
+      <div className="application-history-list">
+        <div className="column">
+          <h2 className="application-history-title">Position</h2>
+          {positions.map((position, index) => (
+            <p key={index}>{position}</p>
+          ))}
+        </div>
+        <div className="column">
+          <h2 className="application-history-title">Company</h2>
+          {companies.map((company, index) => (
+            <p key={index}>{company}</p>
+          ))}
+        </div>
+        <div className="column">
+          <h2 className="application-history-title">Location</h2>
+          {locations.map((location, index) => (
+            <p key={index}>{location}</p>
+          ))}
+        </div>
+        <div className="column">
+          <h2 className="application-history-title">Contract</h2>
+          {contracts.map((contract, index) => (
+            <p key={index}>{contract}</p>
+          ))}
+        </div>
+        <div className="column">
+          <h2 className="application-history-title">Action</h2>
+          {positions.map((_, index) => (
+            <div key={index} className="action-container">
+              <button onClick={() => handleOptionsClick(index)}>...</button>
+              {showOptions[index] && (
+                <div className="options-container">
+                  <button>View Application</button>
+                  <button>Withdraw Application</button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
+
+
 
 
 export default ApplicationHistory;
