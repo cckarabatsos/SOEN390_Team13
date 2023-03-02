4ca4c17be9227e14fcd18bba88be5c3cd6e8f041
SkylineP <77853117+SkylineP@users.noreply.github.com>
2023-03-01 22:18:40 -0500

    Connected User Profile with Database
    Connected User Profile with Database, can now fetch all user info #176

--- SOEN390_APP/Pages/UserProfile.tsx
+++ SOEN390_APP/Pages/UserProfile.tsx
@@ -14,13 +14,26 @@ import {
 import React from 'react'
 import { useEffect, useState } from "react";
 import Basic from '../Components/SwipeList.Component/SwipeListBasic.Component';
-import StandaloneRow from '../Components/SwipeList.Component/SwipeListEdit.Component';
+import StandaloneRowSkills from '../Components/SwipeList.Component/SwipeListSkills.Component';
+import StandaloneRowExperience from '../Components/SwipeList.Component/SwipeListExperience.Component';
+import StandaloneRowEducation from '../Components/SwipeList.Component/SwipeListEducation.Component';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { LogBox } from 'react-native';
+import { GetUserInfo } from '../api/GetUsersAPI';
+import { GetUserExperience } from '../api/UserExperienceAPI';
+import { GetUserSkills } from '../api/UserSkillsAPI';
 
-
-
+interface User {
+  id: number;
+  name: string;
+  occupation: string;
+  location: string;
+  company: string;
+  image: string;
+  email: string
+  userID: String
+}
 
 const ExpandableComponent = ({item, onClickFunction}:any) => {
     //Custom Component for the Expandable List
@@ -38,7 +51,7 @@ const ExpandableComponent = ({item, onClickFunction}:any) => {
       }
     }, [item.isExpanded]);
   
-    if(item.category_name == "Profile" || item.category_name == "Skills"){
+    if(item.category_name == "Profile"){
     return (
       <View>
         {/*Header of the Expandable List Item*/}
@@ -46,7 +59,7 @@ const ExpandableComponent = ({item, onClickFunction}:any) => {
           activeOpacity={0.8}
           onPress={onClickFunction}
           style={styles.header}>
-            <Ionicons size={20} name="add-circle"/>
+            <Ionicons size={20} name="list-outline"/>
           <Text style={styles.headerText}>
             {item.category_name}
           </Text>
@@ -64,7 +77,7 @@ const ExpandableComponent = ({item, onClickFunction}:any) => {
       </View>
     );
           }
-          else{
+          else if (item.category_name == "Experience"){
              return (
       <View>
         {/*Header of the Expandable List Item*/}
@@ -72,7 +85,7 @@ const ExpandableComponent = ({item, onClickFunction}:any) => {
           activeOpacity={0.8}
           onPress={onClickFunction}
           style={styles.header}>
-            <Ionicons size={20} name="add-circle"/>
+            <Ionicons size={20} name="list-outline"/>
           <Text style={styles.headerText}>
             {item.category_name}
           </Text>
@@ -84,22 +97,163 @@ const ExpandableComponent = ({item, onClickFunction}:any) => {
           }}>
           {/*Content under the header of the Expandable List Item*/}
           {item.subcategory.map((item:any, key:any) =>(
-            <StandaloneRow data = {item}/>
+            <StandaloneRowExperience data = {item}/>
           ))}
         </View>
       </View>
     );
           }
-  };
-  
-const UserProfile = ({route}:{route:any}) => {
+    else if (item.category_name == "Education"){
+            return (
+      <View>
+      {/*Header of the Expandable List Item*/}
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={onClickFunction}
+        style={styles.header}>
+          <Ionicons size={20} name="list-outline"/>
+        <Text style={styles.headerText}>
+          {item.category_name}
+        </Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          height: layoutHeight,
+          overflow: 'hidden',
+        }}>
+        {/*Content under the header of the Expandable List Item*/}
+        {item.subcategory.map((item:any, key:any) =>(
+          <StandaloneRowEducation data = {item}/>
+        ))}
+      </View>
+      </View>
+      );
+                }
+      else if (item.category_name == "Skills"){
+                  return (
+            <View>
+            {/*Header of the Expandable List Item*/}
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={onClickFunction}
+              style={styles.header}>
+                <Ionicons size={20} name="list-outline"/>
+              <Text style={styles.headerText}>
+                {item.category_name}
+              </Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                height: layoutHeight,
+                overflow: 'hidden',
+              }}>
+              {/*Content under the header of the Expandable List Item*/}
+              {item.subcategory.map((item:any, key:any) =>(
+                <StandaloneRowSkills data = {item}/>
+              ))}
+            </View>
+            </View>
+            );
+                      }
+        };
+        
 
-        let name = route.params.username
-        let password = route.params.password
-        if(password==null)
-          password="--hidden--"
-        let email = route.params.email
-      
+const UserProfile = ({route}:{route:any}) => {
+  const [user, setUser] = useState({});
+  const [data, setData] = useState([]);
+
+  let name = route.params.username
+  let password = route.params.password
+  if(password==null)
+    password="--hidden--"
+  let email = route.params.email
+  let userID = route.params.userID
+
+  const handleGetUser = async () => {
+    console.log("INSIDE---------------------------------")
+    const user = await GetUserInfo(userID)
+    console.log(user)
+    setUser(user);
+  }
+
+
+  useEffect(() => {
+    handleGetUser();
+    handleGetExperience();
+    handleGetEducation();
+    handleGetSkills();
+  }, []);
+
+  const handleGetExperience = async () => {
+    const user = await GetUserExperience(userID, "Work")
+    const newObjectsArray = user.map(buildObjectExperience);
+    setData(newObjectsArray);
+    // Update the subcategory in CONTENT with the fetched data
+    const updatedContent = [...CONTENT];
+    updatedContent[1].subcategory = newObjectsArray;
+    setListDataSource(updatedContent);
+  }
+
+  const buildObjectExperience = (jsonObject:any) => {
+    const {experienceID, company, atPresent, ownerID, position, startDate, type,endDate } = jsonObject;
+    const obj = {
+      experienceID: experienceID,
+      atPresent: atPresent,
+      company: company,
+      ownerID: ownerID,
+      position: position,
+      startDate: startDate,
+      endDate: endDate,
+      type: type
+    }
+    return obj;
+  }
+
+  const handleGetEducation = async () => {
+    const user = await GetUserExperience(userID, "Education")
+    const newObjectsArray = user.map(buildObjectEducation);
+    setData(newObjectsArray);
+    // Update the subcategory in CONTENT with the fetched data
+    const updatedContent = [...CONTENT];
+    updatedContent[2].subcategory = newObjectsArray;
+    setListDataSource(updatedContent);
+  }
+
+  const buildObjectEducation = (jsonObject:any) => {
+    const {experienceID, company, atPresent, ownerID, position, startDate, type,endDate } = jsonObject;
+    const obj = {
+      experienceID: experienceID,
+      atPresent: atPresent,
+      company: company,
+      ownerID: ownerID,
+      position: position,
+      startDate: startDate,
+      endDate: endDate,
+      type: type
+    }
+    return obj;
+  }
+
+
+  const handleGetSkills = async () => {
+    const user = await GetUserSkills(userID)
+    const newObjectsArray = user.map(buildObjectSkills);
+    setData(newObjectsArray);
+    // Update the subcategory in CONTENT with the fetched data
+    const updatedContent = [...CONTENT];
+    updatedContent[3].subcategory = newObjectsArray;
+    setListDataSource(updatedContent);
+  }
+
+  const buildObjectSkills = (jsonObject:any) => {
+    const {name, skillID, ownerID} = jsonObject;
+    const obj = {
+      name: name,
+      ownerID: ownerID,
+      skillID: skillID
+    }
+    return obj;
+  }
         const CONTENT = [
           {
               isExpanded: false,
@@ -113,26 +267,17 @@ const UserProfile = ({route}:{route:any}) => {
           {
             isExpanded: false,
             category_name: 'Experience',
-            subcategory: [
-              {key: 5, text: 'Software Development Engineer'},
-              {key: 6, text: 'Backend Engineer Intern'},
-            ],
+            subcategory: [],
           },
           {
             isExpanded: false,
             category_name: 'Education',
-            subcategory: [
-              {key: 7, text: 'Concordia University'},
-              {key: 8, text: 'Dawson College'},
-            ],
+            subcategory: [],
           },
           {
               isExpanded: false,
               category_name: 'Skills',
-              subcategory: [
-                {key: 9, input: "Skills", text: 'Java'},
-                {key: 10, input: "Skills", text: 'Python'},
-              ],
+              subcategory: [],
             },
         ];
 
@@ -165,11 +310,11 @@ const UserProfile = ({route}:{route:any}) => {
             <View style={{backgroundColor: "rgb(208, 164, 245)", height: 225, width: '100%',padding: 10}}>
                 <Image
                 style={styles.logo}
-                source={require('../Components/Images/logInBackground.png')}
+                source={{ uri: user.picture }}
                 />
-                <Text style={styles.titleText}> {name} </Text>
-                <Text style={styles.textSmall}> Software Engineering Student </Text>
-                <Text style={styles.textSmall}> Montreal, Quebec </Text>
+                <Text style={styles.titleText}> {user.name} </Text>
+                <Text style={styles.textSmall}> {user.currentCompany + " " + user.currentPosition} </Text>
+                <Text style={styles.textSmall}> {user.bio} </Text>
             </View>
 
         </View>
--- SOEN390_APP/api/GetUsersAPI.tsx
+++ SOEN390_APP/api/GetUsersAPI.tsx
@@ -14,3 +14,17 @@ export async function GetUsersAPI(data:any) {
     return false;
   }
 }
+
+  export async function GetUserInfo(data:any) {
+    try {
+      const response = await axios.get(api.BACKEND_API + "/user/id/:userID", {
+        params: {
+          userID: data
+        },
+      });
+      return response.data.data[1];
+    } catch (error) {
+      console.log("error", error);
+      return false;
+    }
+}
