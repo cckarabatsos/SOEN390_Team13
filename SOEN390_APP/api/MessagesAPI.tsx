9251ca79f12f93c62c0fe452c98bd68100f26ebe
SkylineP <77853117+SkylineP@users.noreply.github.com>
2023-03-11 16:13:07 -0500

    #178

--- SOEN390_APP/api/MessagesAPI.tsx
+++ SOEN390_APP/api/MessagesAPI.tsx
@@ -24,7 +24,7 @@ export async function CreateConversation(emailUser:string, emailContact:string) 
   }
 
   export async function GetAllMessages(emailUser:string, emailContact:string) {
-    console.log("INSIDE messages" )
+    //console.log("INSIDE messages" )
     //console.log(emailUser, emailContact)
     //console.log(JSON.stringify([emailUser, emailContact]))
     try {
@@ -34,9 +34,6 @@ export async function CreateConversation(emailUser:string, emailContact:string) 
             senderEmail: emailUser
         },
       });
-      console.log("------------------------" )
-      console.log(response.data.usersChat[1])
-      console.log("------------------------" )
       if (response.status == 200) {
         return response.data.usersChat[1];
       } else {
@@ -66,5 +63,28 @@ export async function CreateConversation(emailUser:string, emailContact:string) 
     } catch (error) {
       console.error("error Remove Application", error);
       return false;
+    }
+  }
+
+  export async function GetActiveConversations(emailUser:string) {
+    //console.log("INSIDE active convos" )
+    //console.log(emailUser, emailContact)
+    //console.log(JSON.stringify([emailUser, emailContact]))
+    try {
+      const response = await axios.get(api.BACKEND_API + "/messages/getAllMessages/", {
+        params: {
+            email: emailUser
+        },
+      });
+      console.log("------------------------" )
+      console.log(response.data)
+      console.log("------------------------" )
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        return response.data;
+      }
+    } catch (error) {
+      console.error("error Remove Application", error);
     }
   }
