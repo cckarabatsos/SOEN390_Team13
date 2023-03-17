2a397b4c75db6e993a6e8a109f1bc6e915982715
Bogdanpoda <68243088+Bogdanpoda@users.noreply.github.com>
2023-03-17 16:26:13 -0400

    added open position component to the company profiel

--- soen390_web/src/pages/CompanyProfile.jsx
+++ soen390_web/src/pages/CompanyProfile.jsx
@@ -3,6 +3,7 @@ import "../styles/components/FileItem.css";
 import CompanyHeader from "../components/CompanyHeader";
 import CompanyDescription from "../components/CompanyDescription";
 import CompanyApplicationList from "../components/CompanyApplicationList";
+import CompanyJobPostings from "../components/CompanyJobPostings";
 
 
 export default function CompanyProfilePage(props){
@@ -11,9 +12,8 @@ export default function CompanyProfilePage(props){
         <div style={{minHeight:"90vh"}}>
              <CompanyHeader></CompanyHeader>
         <CompanyDescription></CompanyDescription>
+        <CompanyJobPostings></CompanyJobPostings>
         <CompanyApplicationList></CompanyApplicationList>
-
-
         </div>
        
     )
