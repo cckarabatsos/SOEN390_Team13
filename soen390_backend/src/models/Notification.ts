eccde032b8e263c2ef958bb5eb4d660214b34808
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2023-03-22 15:37:32 -0400

    Created Notificaiton model and notifications array on User model

---
+++ soen390_backend/src/models/Notification.ts
@@ -0,0 +1,16 @@
+/**
+ * Model for the Notification entity
+ */
+import * as yup from "yup";
+//Some types are to be changed later
+export const notification_schema = yup
+    .object()
+    .shape({
+        logo: yup.string().required(),
+        timestamp: yup.date().required(),
+        message: yup.string().required(),
+        category: yup.string().required()
+    })
+    .required();
+
+export type Notification = yup.InferType<typeof notification_schema>;
