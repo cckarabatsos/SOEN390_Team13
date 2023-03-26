1f73d378f02ecc106950f384edec9e2fedc48c6e
SkylineP <77853117+SkylineP@users.noreply.github.com>
2023-03-26 17:12:53 -0400

    Added notifications API #310

---
+++ SOEN390_APP/api/NotificationAPI.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import api from "../config.json";
+
+export async function GetNotification(userId:string) {
+  try {
+    const response = await axios.get(
+      api.BACKEND_API + '/notification/getNotifications/'+userId
+    );
+    return response.data;
+  } catch (error) {
+    console.error("error", error);
+    return [];
+  }
+}
+
+export async function removeNotification(notificationId:string){
+    try {
+        const response = await axios.get(
+          api.BACKEND_API + '/notification/remove/'+notificationId
+        );
+
+        if(response.status == 200){
+            return true
+        }
+        else{
+            return false
+        }
+      } catch (error) {
+        console.error("error", error);
+        return false
+      }
+}
