73912e7007fb9e8351f3cdbe79872617713813b7
nadiaaab <90936367+nadiaaab@users.noreply.github.com>
2023-04-01 14:29:30 -0400

    comments added
    comments added to userconnectioncomponent

--- soen390_web/src/__tests__/UserConnectionComponent.test.jsx
+++ soen390_web/src/__tests__/UserConnectionComponent.test.jsx
@@ -1,4 +1,6 @@
+// import dependencies
 import React from "react";
+// import react-testing methods
 import { render, fireEvent } from "@testing-library/react";
 import UserConectionComponent from "../components/UserConectionComponent";
 
@@ -14,19 +16,23 @@ const props = {
 };
 
 describe("UserConnectionComponent", () => {
-  it("should render user connection details correctly", () => {
-    const { getByText } = render(<UserConectionComponent {...props} />);
-    expect(getByText("name")).toBeInTheDocument();
-    expect(getByText("job")).toBeInTheDocument();
-    expect(getByText("2 mutual friends")).toBeInTheDocument();
+    
+    //checks that user connection details are rendered correctly
+    it("should render user connection details correctly", () => {
+        const { getByText } = render(<UserConectionComponent {...props} />);
+        expect(getByText("name")).toBeInTheDocument();
+        expect(getByText("job")).toBeInTheDocument();
+        expect(getByText("2 mutual friends")).toBeInTheDocument();
   });
-
+  
+  //checks that accept function is called when clicking on accept button
   it("should call accept function when clicking on accept button", () => {
     const { getByText } = render(<UserConectionComponent {...props} />);
     fireEvent.click(getByText("AcceptText"));
     expect(props.accept).toHaveBeenCalledWith("current@example.com", "sender@example.com");
   });
 
+  //checks that decline function is called when clicking on decline button
   it("should call decline function when clicking on decline button", () => {
     const { getByText } = render(<UserConectionComponent {...props} />);
     fireEvent.click(getByText("DeclineText"));
